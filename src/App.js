46c93fdebb0533ffd0787b92302098fdddbd24cb
MMostella <masonmostella@gmail.com>
2021-08-20 11:16:08 -0500

    Added styling to home and app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import { Route, Switch, Link } from 'react-router-dom';
 import Home from "./components/Home";
 import Form from "./components/Form";
 
+import styled from 'styled-components';
+
+const StyledApp = styled.div `
+  .headerMain {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .headerMain h1 {
+    color: red;
+    margin-left: 5%;
+    font-size: 2.5em;
+  }
+
+  .homeLink {
+    margin-right: 5%;
+    text-decoration: none;
+    font-size: 1.8em;
+  }
+`
 
 const App = () => {
   return (
-    <><div>
+    <StyledApp>
+      <div>
         <h1>Lambda Eats</h1>
         <Link to='/' className='homeLink'>Home</Link>
       </div>
@@ -18,7 +40,7 @@ const App = () => {
           <Home />
         </Route>
       </Switch>      
-    </>
+    </StyledApp>
   );
 };
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,109 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styled from 'styled-components';
+
+const StyledHome = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .homePic {
+        background: url('https://picsum.photos/seed/picsum/2500/2500');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        width: 100%;
+        height: 500px;
+    }
+
+    .homepage {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        top: 275px;
+    }
+
+    .homepage h2 {
+        font-size: 3rem;
+    }
+
+    .pizzaButton {
+        width: 15%;
+        height: 2.5em;
+        font-size: 1em;
+        font-weight: bold;
+    }
+
+    .delivery h3 {
+        font-size: 1.5em;
+    }
+
+    .foodPlaces {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        width: 95%;
+        height: 600px;
+    }
+
+    .group-place {
+        display: flex;
+        flex-direction: column;
+        width: 25%;
+        height: 300px;
+        margin: 2%;
+    }
+
+    .group-place img {
+        height: 375px;
+        background-position: center;
+        border: 2.5px solid black;
+        border-radius: 25px;
+    }
+
+    .place {
+        margin: 1% 0 1% 5%;
+    }
+
+    .place h4 {
+        font-weight: bold;
+    }
+
+    .place button {
+        margin: 0 1%;
+    }
+
+    .mcdonalds {
+        background: url('https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+    }
+    .olive {
+        background: url('https://images.unsplash.com/photo-1498579150354-977475b7ea0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aXRhbGlhbiUyMGZvb2R8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+    }
+    .mill {
+        background: url('https://images.unsplash.com/photo-1542128722-d6fe34923abc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdyYXB8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+    }
+    .rosies {
+        background: url('https://images.unsplash.com/photo-1464219222984-216ebffaaf85?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bWV4aWNhbiUyMGZvb2R8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+    }
+    .blue {
+        background: url('https://images.unsplash.com/photo-1600688685721-852c38f6e8a6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3Jhdnl8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+    }
+    .viet {
+        background: url('https://images.unsplash.com/photo-1602253057119-44d745d9b860?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXNpYW4lMjBmb29kfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+    }
+
+    .divider {
+        border-bottom: 10px solid black;
+    }
+`
+
 export default function Home() {
     return (
-        <>
+        <StyledHome>
             <img className='homePic'/>
             <div className='homepage'>
                 <h2>The food you want... when you want it!</h2>
@@ -70,6 +170,6 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-        </>
+        </StyledHome>
     )
 }
