db4d52d9fe25900c1a662252d1af197387c58615
MMostella <masonmostella@gmail.com>
2021-08-20 09:14:26 -0500

    Set up routes and links in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Switch, Link } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    <><div>
+        <h1>Lambda Eats</h1>
+        <Link to='/' className='homeLink'>Home</Link>
+      </div>
+      <Switch>
+        <Route exact path='/pizza'>
+          {/* <Form /> */}
+        </Route>
+        <Route exact path='/'>
+          {/* <Home /> */}
+        </Route>
+      </Switch>      
     </>
   );
 };
